c11566996907d2dfb5fe8dca4772ede748e482ec
Cynthia Uche <cynthianuchepro@gmail.com>
2025-02-11 00:44:06 +0100

    feat: add GET /api/v1/books/{book_id} endpoint

--- api/routes/books.py
+++ api/routes/books.py
@@ -60,3 +60,11 @@ async def update_book(book_id: int, book: Book) -> Book:
 async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
+
+
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book_by_id(book_id: int):
+    book = db.get_book(book_id)
+    if not book:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"detail": "Book not found"})
+    return book
